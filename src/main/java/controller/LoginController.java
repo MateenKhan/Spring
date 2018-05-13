8aebd97a8212cc85576a5b4811f6c085a038346b
MateenKhan <mateen.khan@qount.io>
2018-05-14 04:32:03 +0530

    db auth

--- src/main/java/controller/LoginController.java
+++ src/main/java/controller/LoginController.java
@@ -1,24 +1,24 @@
-package controller;
-
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.servlet.ModelAndView;
-
-@Controller
-public class LoginController {
-	@RequestMapping("/login")
-	public ModelAndView helloWorld(HttpServletRequest request, HttpServletResponse res) {
-		String name = request.getParameter("name");
-		String password = request.getParameter("password");
-
-		if (password.equals("admin")) {
-			String message = "HELLO " + name;
-			return new ModelAndView("result", "message", message);
-		} else {
-			return new ModelAndView("result", "message", "Sorry, username or password error");
-		}
-	}
-}
+//package controller;
+//
+//import javax.servlet.http.HttpServletRequest;
+//import javax.servlet.http.HttpServletResponse;
+//
+//import org.springframework.stereotype.Controller;
+//import org.springframework.web.bind.annotation.RequestMapping;
+//import org.springframework.web.servlet.ModelAndView;
+//
+//@Controller
+//public class LoginController {
+//	@RequestMapping("/login")
+//	public ModelAndView helloWorld(HttpServletRequest request, HttpServletResponse res) {
+//		String name = request.getParameter("name");
+//		String password = request.getParameter("password");
+//
+//		if (password.equals("admin")) {
+//			String message = "HELLO " + name;
+//			return new ModelAndView("result", "message", message);
+//		} else {
+//			return new ModelAndView("result", "message", "Sorry, username or password error");
+//		}
+//	}
+//}
--- src/main/resources/db.sql
+++ src/main/resources/db.sql
@@ -1,7 +1,26 @@
-CREATE TABLE `spring`.`emp99`(  
-  `id` INT(20) NOT NULL AUTO_INCREMENT,
-  `name` VARCHAR(4000),
-  `salary` INT(20),
-  `designation` VARCHAR(4000),
-  PRIMARY KEY (`id`)
-) ENGINE=INNODB CHARSET=utf8 COLLATE=utf8_general_ci;
+CREATE  TABLE users (
+  username VARCHAR(45) NOT NULL ,
+  password VARCHAR(45) NOT NULL ,
+  enabled TINYINT NOT NULL DEFAULT 1 ,
+  PRIMARY KEY (username));
+  
+  CREATE TABLE user_roles (
+  user_role_id int(11) NOT NULL AUTO_INCREMENT,
+  username varchar(45) NOT NULL,
+  role varchar(45) NOT NULL,
+  PRIMARY KEY (user_role_id),
+  UNIQUE KEY uni_username_role (role,username),
+  KEY fk_username_idx (username),
+  CONSTRAINT fk_username FOREIGN KEY (username) REFERENCES users (username));
+
+  INSERT INTO users(username,password,enabled)
+VALUES ('mkyong','123456', true);
+INSERT INTO users(username,password,enabled)
+VALUES ('alex','123456', true);
+
+INSERT INTO user_roles (username, role)
+VALUES ('mkyong', 'ROLE_USER');
+INSERT INTO user_roles (username, role)
+VALUES ('mkyong', 'ROLE_ADMIN');
+INSERT INTO user_roles (username, role)
+VALUES ('alex', 'ROLE_USER');
